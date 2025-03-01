dfea6209f6a82a39d5ef2b0bfb170ae8e8bbcb26
jatinprime <jatinagrawal28112003@gmail.com>
2025-03-01 12:17:10 +0530

    Enhance server setup with dotenv, middleware integration, and improved logging

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-const express = require('express') ;
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 
-//rest object 
-const app = express() ;
+//dotenv config
+//we do not require path , as server.js is already present in the root directory with .env file , so no need to give path in dotenv.confi
+dotenv.config();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 // route
-app.get('/' , (req , res) => {
-    return res
-    .status(200)
-    .send("<h1>Welcome to food app</h1>")
-})
+app.get("/", (req, res) => {
+    return res.status(200).send("<h1>Welcome to food app</h1>");
+});
 
 // port
-const PORT = 8000 ;
+const PORT = process.env.PORT || 8000;
 
 //listen
-app.listen(PORT , () => {
-    console.log("Server Running Successfully") ;
-})
+app.listen(PORT, () => {
+    console.log(`Server Running Successfully on ${PORT}`.white.bgMagenta);
+});
