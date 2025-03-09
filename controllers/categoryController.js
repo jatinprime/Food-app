132a70c450ac640d2270556aae11d89dab740d1d
jatinprime <jatinagrawal28112003@gmail.com>
2025-03-09 20:23:06 +0530

    Add category management functionality with CRUD operations and update VSCode settings

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -29,4 +29,90 @@ const createCategoryController = async (req , res) => {
     }
 }
 
-module.exports = {createCategoryController}
+//GET ALL CATEGORY
+const getAllCategoryController = async(req , res) => {
+    try{
+        const categories = await categoryModel.find({}) ;
+        if(!categories){
+            return res.status(404).send({
+                success : false , 
+                message : "No Categories Found",
+            }) ;
+        }
+        res.status(200).send({
+            success : true , 
+            totalCat : categories.length , 
+            categories,
+        }) ;
+    }catch(error){
+        console.log(error) ;
+        res.status(500).send({
+            success : false , 
+            message : "Error in get All Category API",
+            error
+        })
+    }
+}
+
+//UPDATE CATEGORY
+const updateCategoryController = async(req , res) => {
+    try{
+        const {id} = req.params ;
+        const {title , imageUrl} = req.body ;
+        const updatedCategory = await categoryModel.findByIdAndUpdate(id , {title , imageUrl} , {new : true}) ;
+        if(!updatedCategory){
+            return res.status(500).send({
+                success : false , 
+                message : "No Category Found" 
+            })
+        }
+
+        res.status(200).send({
+            success : true , 
+            message : "Category Updated Successfully",
+        });
+    }catch(error){
+        console.log(error) ;
+        res.status(500).send({
+            success : false , 
+            message : "Error in Update Category API",
+            error
+        })
+    }
+}
+
+//DELETE CATEGORY
+const deleteCategoryController = async(req , res) => {
+    try{
+        const {id} = req.params ;
+        if(!id){
+            return res.status(500).send({
+                success : false ,
+                message : "Please provide Category ID"
+            })
+        }
+
+        const category = await categoryModel.findById(id) ;
+        if(!category){
+            return res.status(500).send({
+                success : false , 
+                message : "No Category Found with this id",
+            });
+        }
+
+        await categoryModel.findByIdAndDelete(id) ;
+        res.status(200).send({
+            success : true , 
+            message : "Deleted Successfully"
+        })
+    }catch(error){
+        console.log(error) ;
+        res.status(500).send({
+            success : false , 
+            message : "Error in Delete Category API",
+            error
+        })
+    }
+}
+
+module.exports = {createCategoryController , getAllCategoryController , updateCategoryController , deleteCategoryController} ;
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,12 +1,21 @@
 const express = require('express') ;
 
 const {verifyJWT} = require("../middlewares/authMiddleware") ;
-const { createCategoryController } = require("../controllers/categoryController");
+const { createCategoryController, getAllCategoryController, updateCategoryController, deleteCategoryController } = require("../controllers/categoryController");
 
 const router = express.Router() ;
 
 //Routes
 //Create Category
 router.post("/create" , verifyJWT , createCategoryController) ;
+
+//GET ALL CATEGORY
+router.get("/getAll" , getAllCategoryController) ;
+
+//UPDATE CATEGORY
+router.put("/update/:id" , verifyJWT , updateCategoryController) ;
+
+//DELETE CATEGORY
+router.delete("/delete/:id" , verifyJWT , deleteCategoryController)
 
 module.exports = router ;
