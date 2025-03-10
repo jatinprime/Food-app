aae214de2e712ed9c9887e4f7d101cca7204cd8d
jatinprime <jatinagrawal28112003@gmail.com>
2025-03-11 00:42:31 +0530

    Add order model with schema for managing orders and their statuses

---
+++ models/orderModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+//schema
+const ordersSchema = new mongoose.Schema(
+    {
+        //MAKING ARRAY FOR FOODS , AS MULTIPLE ITEMS CAN BE THERE 
+        foods: [{ type: mongoose.Schema.Types.ObjectId, ref: "Foods" }],
+        payment: {},
+        buyer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        status: {
+            type: String,
+            enum: ["preparing", "prepare", "on the way", "deliverd"],
+            default: "preparing",
+        },
+    },
+    { timestamps: true }
+);
+
+//export
+module.exports = mongoose.model("Orders", ordersSchema);
